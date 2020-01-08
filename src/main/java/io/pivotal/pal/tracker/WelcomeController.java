e9301ec63a82493309f1f6bc0b14e763c3d86baa
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:04 -0500

    Add tests for deployment lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    String WELCOME_Message ;
+
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.WELCOME_Message = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+
+        return WELCOME_Message;
     }
 }
